a1d17fc40a464dc4f56f29cffe7ec3e65ddda1ef
Tanya Lakhman <Tanya2202@ukr.net>
2018-10-24 13:35:48 +0300

    maps+footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 jQuery(document).ready(function($) {
     $('.responsive').slick({
         dots: true,
-        infinite: false,
+        infinite: true,
         speed: 300,
         slidesToShow: 5,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
     });
+
+    $("#contactsPhone").mask("+73 (99)999-99-99", {placeholder: ""});
 });
