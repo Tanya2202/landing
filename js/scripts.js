ad18f8b37bbad6cdb95425a465d63d71bbe3d537
Tanya Lakhman <Tanya2202@ukr.net>
2018-10-30 13:50:59 +0200

    Latest version

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,46 @@ jQuery(document).ready(function($) {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 
     $("#contactsPhone").mask("+73 (99)999-99-99", {placeholder: ""});
+
+    // Toggle menu on small screens
+    $('.menuBtnForSmScreens').click(function() {
+        if ($('#navbar').is(':visible')) {
+            $('#navbar').slideUp();
+        } else {
+            $('#navbar').slideDown();
+        }
+    });
 });
